9d228afc669938fe052ec58e3c1a3be3e521fa61
Adam Toennis <adam@atomicrobot.com>
2022-03-04 15:32:51 -0500

    Initial Commit

---
+++ Shared/ble_peripheral_appleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ble_peripheral_appleApp.swift
+//  Shared
+//
+//  Created by Adam Toennis on 3/4/22.
+//
+
+import SwiftUI
+
+@main
+struct ble_peripheral_appleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
